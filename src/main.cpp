917367611b56bd13cd770e93f453df58404cf1a8
Yuxuan Tian <yuxuan.tian@cscad.com>
2024-08-05 17:45:55 +0800

    update cmake

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,10 @@ int main()
 {
     if (!glfwInit()) {
         std::cout << "init failed" << std::endl;
-        return;
+        return 0;
     }
 
     std::cout << "init successed" << std::endl;
     glfwTerminate();
+    return 0;
 }
